8aff5a38dd10d5f6b00a2bb4d6028aa1f8f5ac49
Flynn <flynn@datawire.io>
2016-06-15 21:59:55 -0400

    Clean up feedback and text.

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 set -e
 
 PYVER=$(python -c "import platform ; print(platform.python_version())" 2>/dev/null)
+echo "= Installing the Datawire MDK"
+
 
 if [ -z "$PYVER" ]; then
   # No python. That's a problem.
   cat <<EOF >&2
-You don't seem to have Python installed! Check out https://www.python.org to
-get that sorted out.
+You don't seem to have Python installed! To get that sorted out,
+check out https://www.python.org.
 EOF
   exit 1
 fi
@@ -16,8 +18,9 @@ fi
 if [ $(echo "$PYVER" | egrep -c '^2\.') -eq 0 ]; then
   # We need python 2.
   cat <<"EOF" >&2
-You seem to have Python $PYVER installed. We only support Python 2, and we need 2.7
-or higher. Check out https://www.python.org to get that sorted out.
+You seem to have Python $PYVER installed. We only support Python 2,
+and we need 2.7 or higher. Check out https://www.python.org to get
+that sorted out.
 EOF
   exit 1
 fi
@@ -25,9 +28,9 @@ fi
 if ! command -v pip >/dev/null 2>&1; then
   # No pip. That's a problem.
   cat <<EOF >&2
-You don't seem to have pip installed. The easiest way to fix this may be
-to install and use virtualenv -- check out https://virtualenv.pypa.io/en/stable for
-more!
+You don't seem to have pip installed. The easiest way to fix this may
+be to install and use virtualenv -- for more information, check out
+https://virtualenv.pypa.io/en/stable.
 EOF
   exit 1
 fi
@@ -48,7 +51,7 @@ echo "== Compiling the MDK"
 quark install --python https://raw.githubusercontent.com/datawire/discovery/dev/2.0/quark/{discovery-2.0.0,datawire_introspection}.q
 
 # Get Python set up.
-echo "== Verifying Flask"
+echo "== Setting up Flask and Requests"
 pip install requests flask
 
 # All done.
