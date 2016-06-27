169c08e45821e29bc1c73d033a4994f961a1ef76
Rafael Schloming <rhs@alum.mit.edu>
2016-06-27 11:45:53 -0400

    Updated test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ logging.basicConfig(level=logging.INFO)
 
 import tracing
 
-logger = tracing.Logger()
-logger.log(logging.DEBUG, "test", "testing...")
+tracer = tracing.Tracer()
+tracer.log("DEBUG", "test", "testing...")
