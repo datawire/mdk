11b340d799f43cf512ff23c7ba2a330e2eaa5167
Flynn <flynn@datawire.io>
2016-06-29 13:45:12 -0400

    Updates to test logging clients

--- test-console.py
+++ test-console.py
@@ -4,19 +4,29 @@ logging.basicConfig(level=logging.DEBUG)
 import tracing
 import time
 
+import quark
+
 # tracer = tracing.Tracer.withURLsAndToken("ws://localhost:52690/ws", None, "fakeToken")
 tracer = tracing.Tracer.withURLsAndToken("wss://tracing-develop.datawire.io/ws", None, None)
 
 def goodHandler(result):
-	print("Good!")
+	# logging.info("Good!")
 
 	for record in result:
-		print(record.toString())
+		logging.info(record.toString())
 		
 def badHandler(result):
-	print("Failure: %s" % result.toString())
+	logging.info("Failure: %s" % result.toString())
 
 def formatLogRecord(record):
 	return("%.3f %s %s" % (record.timestamp, record.record.level, record.record.text))
 
-tracer.poll().andEither(goodHandler, badHandler)
+def poller(hrm=None):
+	# logging.info("POLLING")
+
+	tracer.poll() \
+	      .andEither(goodHandler, badHandler) \
+	      .andFinally(lambda x: quark.IO.schedule(1)) \
+	      .andFinally(poller)
+
+poller()
