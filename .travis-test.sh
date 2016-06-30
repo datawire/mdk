a888ee0cb85f01f77882d0ab012ae6e1a1cab249
Itamar Turner-Trauring <itamar@datawire.io>
2016-06-30 16:06:24 -0400

    Possibly simpler way to not use rvm.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-# BEGONE, rvm!
-PATH=$(echo "$PATH" | tr : \\n |
-              grep -vF -e /.local/ -e /.rvm/ -e /.gem/ |
-              tr \\n : | sed -e's/:$//' )
-rm -fr ~/.rvm
-rm -fr ~/.local
-unset $(env | grep -Fe ~/.rvm | cut -d= -f1)
-echo "install: --user-install" > ~/.gemrc
-echo "gempath at $1 is $(gem env gempath)"
-PATH="$PATH":$(gem env gempath | tr : \\n | grep -F -e "$HOME/.gem/" | head -1)/bin
-
+rvm use system
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 for CODE_LANG in --python --ruby --java --javascript ; do
     curl -# -L https://raw.githubusercontent.com/datawire/mdk/$GIT_BRANCH/install.sh | bash -s -- $CODE_LANG $GIT_BRANCH
