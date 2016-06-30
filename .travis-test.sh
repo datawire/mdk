6f4e42c93c03b4bd6cad0c0f97172ca0ec107bc1
Itamar Turner-Trauring <itamar@datawire.io>
2016-06-30 16:39:46 -0400

    UUUUUUUUU

--- .travis-test.sh
+++ .travis-test.sh
@@ -6,7 +6,7 @@ export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "Git branch is:" $GIT_BRANCH
 for CODE_LANG in --python --ruby --java --javascript ; do
     echo "Language is:" $CODE_LANG
-    ./install.sh $CODE_LANG $GIT_BRANCH
+    bash install.sh $CODE_LANG $GIT_BRANCH
     ~/.quark/bin/quark install $CODE_LANG quark/mdk_test.q
     ~/.quark/bin/quark run $CODE_LANG quark/mdk_test.q
 done
