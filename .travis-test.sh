3ebcea41f9767cf4a4a8759e444b349a9f218ba2
Itamar Turner-Trauring <itamar@datawire.io>
2016-06-30 16:33:33 -0400

    Try something else.

--- .travis-test.sh
+++ .travis-test.sh
@@ -3,8 +3,10 @@ set -e
 set -v
 rvm use system
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo "Git branch is:" $GIT_BRANCH
 for CODE_LANG in --python --ruby --java --javascript ; do
-    curl -# -L https://raw.githubusercontent.com/datawire/mdk/$GIT_BRANCH/install.sh | bash -s -- $CODE_LANG $GIT_BRANCH
+    echo "Language is:" $CODE_LANG
+    ./install.sh $CODE_LANG $GIT_BRANCH
     ~/.quark/bin/quark install $CODE_LANG quark/mdk_test.q
     ~/.quark/bin/quark run $CODE_LANG quark/mdk_test.q
 done
