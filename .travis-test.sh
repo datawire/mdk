61260935531de6e6f95a8b211f7742696c945783
Itamar Turner-Trauring <itamar@datawire.io>
2016-06-30 16:45:23 -0400

    Try using commit rather than guessing branch.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
-set -v
 rvm use system
-export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-echo "Git branch is:" $GIT_BRANCH
+echo "Git commit is:" $TRAVIS_COMMIT
 for CODE_LANG in --python --ruby --java --javascript ; do
     echo "Language is:" $CODE_LANG
-    bash install.sh $CODE_LANG $GIT_BRANCH
+    bash install.sh $CODE_LANG $TRAVIS_COMMIT
     ~/.quark/bin/quark install $CODE_LANG quark/mdk_test.q
     ~/.quark/bin/quark run $CODE_LANG quark/mdk_test.q
 done
