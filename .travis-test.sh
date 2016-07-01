2fd9fd72bbdf30440d4656f38c1025b302d13002
Itamar Turner-Trauring <itamar@datawire.io>
2016-07-01 09:17:51 -0400

    Try to fix Ruby installation.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e
 
 # Remove this when bug is fixed:
 export DATAWIRE_TOKEN=""
 
-set -e
+# Setup local Gem install instead of system install:
 rvm use system
+echo "install: --user-install" > ~/.gemrc
+echo "gempath at $1 is $(gem env gempath)"
+export PATH="$PATH":$(gem env gempath | tr : \\n | grep -F -e "$HOME/.gem/" | head -1)/bin
+
 echo "Git commit is:" $TRAVIS_COMMIT
 for CODE_LANG in --python --ruby --java --javascript ; do
     echo "Language is:" $CODE_LANG
