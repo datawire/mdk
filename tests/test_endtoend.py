1d62c26b2463046f8375e6f9a77b74cc443f038e
Itamar Turner-Trauring <itamar@datawire.io>
2016-07-26 09:52:37 -0400

    Run against production servers, for extra validity.

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -9,10 +9,6 @@ from unittest import TestCase
 CODE_PATH = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "source"))
 
-os.putenv("MDK_DISCOVERY_URL", "wss://discovery-develop.datawire.io/ws/v1")
-os.putenv("MDK_TRACING_URL", "wss://tracing-develop.datawire.io/ws/v1")
-os.putenv("MDK_TRACING_API_URL", "https://tracing-develop.datawire.io/api/v1/logs")
-
 
 def random_string():
     return "random_" + str(random())[2:]
