d84268aa659f07e718cda897fd90016953b19a5b
Itamar Turner-Trauring <itamar@datawire.io>
2016-08-30 16:31:19 -0400

    RubyGems.org support, untested.

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,5 +1,8 @@
 """
 Upload packages to PyPI, RubyGems, NPM and Maven.
+
+We use temporary file to ensure passwords do not get leaked into Travis logs via
+errors or tracebacks.
 """
 
 import os
@@ -17,19 +20,42 @@ username = datawire
 password = {}
 """
 
+GEM_CONFIG = """\
+---
+:rubygems_api_key: {}
+"""
+
 
 def upload_python():
     """
     Upload wheel to PyPI.
-
-    We use temporary file to ensure PyPI password does not get leaked into
-    Travis logs via errors or tracebacks.
     """
     wheel, = glob("dist/datawire_mdk*.whl")
     with NamedTemporaryFile() as f:
         f.write(PYPI_CONFIG.format(os.environ["PYPI_PASSWORD"]))
         f.flush()
         check_call(["twine", "upload", "--config-file", f.name, wheel])
+
+
+def upload_gem():
+    """
+    Upload gem to rubygems.org.
+    """
+    gem, = glob("dist/datawire_mdk*.gem")
+    directory = os.path.expanduser("~/.gem")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    creds = os.path.join(directory, "credentials")
+    creds_existed = os.path.exist(creds)
+    try:
+        if not creds_existed:
+            with open(creds, "w") as f:
+                f.write(GEM_CONFIG.format(os.environ["RUBYGEMS_API_KEY"]))
+        check_call(["gem", "push", gem])
+    finally:
+        if not creds_existed:
+            # Delete the file we wrote:
+            os.remove(creds)
 
 
 def main():
