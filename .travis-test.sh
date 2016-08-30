375f9415ff3535b75435472ee11757c6c7ac6e85
Itamar Turner-Trauring <itamar@datawire.io>
2016-08-30 10:07:26 -0400

    Delete unnecessary comments.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Setup local Gem install instead of system install:
+# Make sure we're using correct version of Ruby:
 rvm use --ruby-version 2.3
-#echo "install: --user-install" > ~/.gemrc
-#echo "gempath at $1 is $(gem env gempath)"
-#export PATH="$PATH":~/.quark/bin/:$(gem env gempath | tr : \\n | grep -F -e "$HOME/.gem/" | head -1)/bin
 
 # Display what ruby we're using, to make sure it's correct:
 ruby --version
