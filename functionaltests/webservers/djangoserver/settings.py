9af2748f391fd4ae5c6b18c23015ce074f66dffd
Itamar Turner-Trauring <itamar@datawire.io>
2016-09-16 12:18:13 -0400

    Implement middleware, add it to test Django setup.

--- functionaltests/webservers/djangoserver/settings.py
+++ functionaltests/webservers/djangoserver/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'mdk.django.MDKSessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
