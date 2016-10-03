3fe10be950f5cb1047dc2c60574026088fc2d7c9
Itamar Turner-Trauring <itamar@datawire.io>
2016-10-03 11:39:54 -0400

    Script for use by Jenkins.

---
+++ scripts/check-if-release.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Check if the current commit is a release commit or not.
+
+This is used by Jenkins to determine whether or not to trigger a build of
+mdk-docs, which needs to be done on every new MDK release (but not on every
+commit).
+"""
+
+from __future__ import print_function
+
+from subprocess import check_output
+from sys import exit
+
+def main():
+    diff = check_output(["git", "diff", "HEAD^", ".bumpversion.cfg"])
+    if "current_version = " in diff:
+        print("Version changed in .bumpversion.cfg, that means this is a release.")
+        exit(0)
+    else:
+        print("No changes to version in .bumpversion.cfg, this is not a release.")
+        exit(1)
+
+main()
