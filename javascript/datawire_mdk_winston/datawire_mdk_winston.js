e8cb026ebed50348784103f545060355027d35e8
Itamar Turner-Trauring <itamar@datawire.io>
2016-11-16 13:12:55 -0500

    Winston transport, the rest of the tests and API.

--- javascript/datawire_mdk_winston/datawire_mdk_winston.js
+++ javascript/datawire_mdk_winston/datawire_mdk_winston.js
@@ -2,17 +2,36 @@ var util = require("util");
 var winston = require("winston");
 var cls = require("datawire_mdk/cls.js");
 
+// Map Winston levels to MDK log levels:
+exports.levelMapping = {
+    "silly": "debug",
+    "debug": "debug",
+    "verbose": "debug",
+    "info": "info",
+    "warn": "warn",
+    "error": "error",
+    "notice": "info",
+    "warning": "warn",
+    "error": "error",
+    "crit": "critical",
+    "alert": "critical",
+    "emerg": "critical"
+};
+
 exports.MDKTransport = function (options) {
+    this.defaultSession = options.mdk.session();
     this.level = options.level || "info";
+    this.name = options.name || this.name;
 };
 util.inherits(exports.MDKTransport, winston.Transport);
-exports.MDKTransport.prototype.name = "MDKTransport";
+exports.MDKTransport.prototype.name = "mdk";
 
 exports.MDKTransport.prototype.log = function (level, msg, meta, callback) {
-    if (typeof meta === 'function') {
-        callback = meta;
-        meta = {};
+    var session = cls.getMDKSession();
+    if (session === undefined) {
+        session = this.defaultSession;
     }
-    cls.getMDKSession().info("", "");
+    level = exports.levelMapping[level];
+    session[level](this.name, msg);
     callback(null, true);
 };
--- test/test_winston.js
+++ test/test_winston.js
@@ -13,7 +13,7 @@ function createMDKwithFakeTracer(level) {
     mdk.start();
     var logger = new (winston.Logger)({
         transports: [
-            new (mdk_winston.MDKTransport)({level: level})
+            new (mdk_winston.MDKTransport)({level: level, mdk: mdk, name: "mycategory"})
         ]
     });
     return {mdk: mdk, tracer: tracer, logger: logger};
@@ -22,21 +22,71 @@ function createMDKwithFakeTracer(level) {
 describe("MDKWinstonTarget", function() {
     it("should use session from getMDKSession", function() {
         var o = createMDKwithFakeTracer("info");
+        assert.strictEqual(o.tracer.messages.length, 0);
         var session = o.mdk.session();
         cls.namespace.run(function() {
             cls.setMDKSession(session);
             o.logger.info("hello!");
         });
+        assert.strictEqual(o.tracer.messages.length, 1);
         assert.strictEqual(o.tracer.messages[0].get("context"),
                            session._context.traceId);
+        assert.strictEqual(o.tracer.messages[0].get("text"), "hello!");
+        assert.strictEqual(o.tracer.messages[0].get("category"), "mycategory");
     });
 
     it("should use default session if there is no current session.", function() {
+        var o = createMDKwithFakeTracer("info");
+        assert.strictEqual(o.tracer.messages.length, 0);
+        o.logger.info("hello2");
+        assert.strictEqual(o.tracer.messages.length, 1);
+        assert.strictEqual(o.tracer.messages[0].get("text"), "hello2");
     });
 
     it("should map npm-style log levels.", function() {
+        var o = createMDKwithFakeTracer("silly");
+        o.logger.transports.mycategory.defaultSession.trace("DEBUG");
+        o.logger.setLevels(winston.config.npm.levels);
+        o.logger.silly("sillyz");
+        o.logger.debug("debugz");
+        o.logger.verbose("verbosez");
+        o.logger.info("infoz");
+        o.logger.warn("warnz");
+        o.logger.error("errorz");
+        assert.deepEqual(
+            ["DEBUG", "DEBUG", "DEBUG", "INFO", "WARN", "ERROR"],
+            o.tracer.messages.map(m => m.get("level")));
+        assert.deepEqual(
+            ["sillyz", "debugz", "verbosez", "infoz", "warnz", "errorz"],
+            o.tracer.messages.map(m => m.get("text")));
     });
 
     it("should map syslog-style log levels.", function() {
+        var o = createMDKwithFakeTracer("debug");
+        o.logger.transports.mycategory.defaultSession.trace("DEBUG");
+        o.logger.setLevels(winston.config.syslog.levels);
+        o.logger.debug("debugz");
+        o.logger.info("infoz");
+        o.logger.notice("noticez");
+        o.logger.warning("warningz");
+        o.logger.error("errorz");
+        o.logger.crit("critz");
+        o.logger.alert("alertz");
+        o.logger.emerg("emergz");
+        assert.deepEqual(
+            ["DEBUG", "INFO", "INFO", "WARN", "ERROR", "CRITICAL", "CRITICAL",
+            "CRITICAL"],
+            o.tracer.messages.map(m => m.get("level")));
+        assert.deepEqual(
+            ["debugz", "infoz", "noticez", "warningz", "errorz", "critz",
+             "alertz", "emergz"],
+            o.tracer.messages.map(m => m.get("text")));
+    });
+
+    it("should have a default name of 'mdk' and default level 'info'.", function() {
+        var o = createMDKwithFakeTracer("debug");
+        var transport = new (mdk_winston.MDKTransport)({mdk: o.mdk});
+        assert.equal(transport.name, 'mdk');
+        assert.equal(transport.level, 'info');
     });
 });
