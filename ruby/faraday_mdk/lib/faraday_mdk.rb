ac2f2780d1dcf66d844751ba588f4593583d573f
Itamar Turner-Trauring <itamar@datawire.io>
2016-11-23 14:00:03 -0500

    Documentation says it should be integer :(

--- ruby/faraday_mdk/lib/faraday_mdk.rb
+++ ruby/faraday_mdk/lib/faraday_mdk.rb
@@ -12,7 +12,14 @@ module FaradayMDK
     def call(request_env)
       request_env[:request_headers]["X-MDK-CONTEXT"] = @mdk_session.externalize
       if @mdk_session.getRemainingTime != nil
-        request_env[:request][:timeout] = @mdk_session.getRemainingTime
+        timeout = Integer(@mdk_session.getRemainingTime)
+        # Rounding down means 0.99 will end up as 0 second timeout, so set
+        # minimum timeout of 1 second:
+        if timeout == 0
+          timeout = 1
+        end
+        request_env[:request][:timeout] = timeout
+        request_env[:request][:open_timeout] = timeout
       end
       @app.call(request_env)
     end
